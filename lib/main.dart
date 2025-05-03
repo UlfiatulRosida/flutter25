98874013507586fe9ca79bad1501dce82bab056a
UlfiatulRosida <ulfiarosidah52@gmail.com>
2025-05-03 12:56:52 +0700

    note

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,19 @@ import 'package:flutter25/pages/album_page.dart';
 import 'package:flutter25/pages/home_page.dart';
 import 'package:flutter25/pages/profile_page.dart';
 import 'package:flutter25/pages/setting_page.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() => runApp(const MainApp());
+const String supabaseKey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl2d21jbWhnempnemR4Ynl6aWx0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUyMjAwMTQsImV4cCI6MjA2MDc5NjAxNH0.pNBrQsyIdWrCUkdfMzn-K8BACKnwO_m0hFKB-yXgrBI';
+
+Future<void> main() async {
+  await Supabase.initialize(
+    url: 'https://yvwmcmhgzjgzdxbyzilt.supabase.co',
+    anonKey: supabaseKey,
+  );
+
+  runApp(const MainApp());
+}
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
@@ -18,6 +29,8 @@ class MainApp extends StatelessWidget {
         '/setting': (context) => SettingPage(),
         '/profile': (context) => ProfilePage(),
         '/album': (context) => AlbumPage(),
+        '/note': (context) => NotePage(),
+        '/note/new': (context) => NoteEditPage(),
       },
       // home: Scaffold(body: Center(child: Text("halo"))),
     );
