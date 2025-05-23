526f269783d92a7c0d3762b5f39c5516c4f001c5
UlfiatulRosida <ulfiarosidah52@gmail.com>
2025-05-24 00:11:26 +0700

    routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter25/pages/album_page.dart';
 import 'package:flutter25/pages/home_page.dart';
+import 'package:flutter25/pages/note_edit_page.dart';
+import 'package:flutter25/pages/note_page.dart';
 import 'package:flutter25/pages/profile_page.dart';
 import 'package:flutter25/pages/setting_page.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:flutter25/pages/note_page.dart';
-import 'package:flutter25/pages/note_edit_page.dart';
 
 const String supabaseKey =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl2d21jbWhnempnemR4Ynl6aWx0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUyMjAwMTQsImV4cCI6MjA2MDc5NjAxNH0.pNBrQsyIdWrCUkdfMzn-K8BACKnwO_m0hFKB-yXgrBI';
@@ -32,7 +32,7 @@ class MainApp extends StatelessWidget {
         '/profile': (context) => ProfilePage(),
         '/album': (context) => AlbumPage(),
         '/note': (context) => NotePage(),
-        '/note/new': (context) => NoteEditPage(),
+        '/note/edit': (context) => NoteEditPage(),
       },
       // home: Scaffold(body: Center(child: Text("halo"))),
     );
